878504f01bd04f035c0039a7ec6a082d5d81c21a
James Miller <jamesrm9235@gmail.com>
2019-05-19 22:20:21 +0900

    11: Implemented direction traversal functions and tested.

--- Python/project_euler/tests/problem_11_test.py
+++ Python/project_euler/tests/problem_11_test.py
@@ -1,19 +1,90 @@
 """https://projecteuler.net/problem=11"""
 
 import unittest
-from project_euler.problem_11 import diagonal_rd, product
+from project_euler.problem_11 import (diagonal_ld, diagonal_lu, diagonal_rd, diagonal_ru,
+                                      down, left, product, right, up)
 
 
 class Problem11Test(unittest.TestCase):
     """Test fixture for problem 11."""
 
+    def test_diagonal_ld(self):
+        """Test that the grid is traversed left-down diagonally from the position."""
+        depth = 4
+        expected = [26, 20, 99, 0]
+        position = (8, 6)
+
+        actual = diagonal_ld(position, depth)
+
+        self.assertListEqual(expected, actual)
+
+    def test_diagonal_lu(self):
+        """Test that the grid is traversed left-up diagonally from the position."""
+        depth = 4
+        expected = [26, 2, 63, 60]
+        position = (8, 6)
+
+        actual = diagonal_lu(position, depth)
+
+        self.assertListEqual(expected, actual)
+
     def test_diagonal_rd(self):
-        """Test that traverses grid right-down diagonally from."""
+        """Test that the grid is traversed right-down diagonally from the position."""
         depth = 4
         expected = [26, 63, 78, 14]
         position = (8, 6)
 
         actual = diagonal_rd(position, depth)
+
+        self.assertListEqual(expected, actual)
+
+    def test_diagonal_ru(self):
+        """Test that the grid is traversed right-up diagonally from the position."""
+        depth = 4
+        expected = [26, 75, 36, 56]
+        position = (8, 6)
+
+        actual = diagonal_ru(position, depth)
+
+        self.assertListEqual(expected, actual)
+
+    def test_down(self):
+        """Test that the grid is traversed downwards from the position."""
+        depth = 4
+        expected = [26, 95, 97, 20]
+        position = (8, 6)
+
+        actual = down(position, depth)
+
+        self.assertListEqual(expected, actual)
+
+    def test_left(self):
+        """Test that the grid is traversed to the left from the position."""
+        depth = 4
+        expected = [26, 10, 67, 23]
+        position = (8, 6)
+
+        actual = left(position, depth)
+
+        self.assertListEqual(expected, actual)
+
+    def test_right(self):
+        """Test that the grid is traversed to the right from the position."""
+        depth = 4
+        expected = [26, 38, 40, 67]
+        position = (8, 6)
+
+        actual = right(position, depth)
+
+        self.assertListEqual(expected, actual)
+
+    def test_up(self):
+        """Test that the grid is traversed upwards from the position."""
+        depth = 4
+        expected = [26, 44, 41, 69]
+        position = (8, 6)
+
+        actual = up(position, depth)
 
         self.assertListEqual(expected, actual)
 
