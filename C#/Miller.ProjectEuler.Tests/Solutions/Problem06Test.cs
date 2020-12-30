db31aa671ab9e4bfad1311d7538dc09444ef37d0
James <jamesrm9235@gmail.com>
2020-12-30 11:36:50 +0900

    Solved problem 6 in C#

---
+++ C#/Miller.ProjectEuler.Tests/Solutions/Problem06Test.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+
+namespace Miller.ProjectEuler.Solutions
+{
+    [TestFixture]
+    public class Problem06Test
+    {
+        [TestCase(10, ExpectedResult = 2640)]
+        [TestCase(100, ExpectedResult = 25164150)]
+        public double Test_Solution(int x)
+        {
+            var sut = new Problem06();
+
+            var actual = sut.Solve(limit: x);
+
+            TestContext.WriteLine(actual);
+            return actual;
+        }
+    }
+}
---
+++ C#/Miller.ProjectEuler/Solutions/Problem06.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Miller.ProjectEuler.Solutions
+{
+    /// <summary>
+    /// Sum square difference
+    /// </summary>
+    /// <seealso cref="https://projecteuler.net/problem=6"/>
+    public class Problem06
+    {
+        /// <summary>
+        /// Iterates through every number up to and including the <paramref name="limit"/> to generate 2 sums:
+        /// <para>
+        /// A sum of the numbers
+        /// </para>
+        /// <para>
+        /// A sum generated from the square of each number
+        /// </para>
+        /// </summary>
+        /// <param name="limit">The number to count towards. The count is inclusive of this number.</param>
+        /// <returns>
+        /// The difference between the square of the sum and sum of the square.
+        /// </returns>
+        /// <remarks>
+        /// 
+        /// </remarks>
+        public double Solve(int limit)
+        {
+            var sum = 0;
+            double sumOfSquare = 0;
+
+            for (int i = 1; i <= limit; i++)
+            {
+                sum += i;
+
+                sumOfSquare += Math.Pow(i, 2);
+            }
+
+            var squareOfSum = Math.Pow(sum, 2);
+
+            return squareOfSum - sumOfSquare;
+        }
+    }
+}
