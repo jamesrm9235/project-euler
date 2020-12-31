427c8e61688348cd748671bfa12e4dd94aa1fea9
James <jamesrm9235@gmail.com>
2020-12-31 20:27:38 +0900

    Solved problem 8 in C#

---
+++ C#/Miller.ProjectEuler.Tests/Solutions/Problem08Test.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+
+namespace Miller.ProjectEuler.Solutions
+{
+    [TestFixture]
+    public class Problem08Test
+    {
+        [TestCase(4, ExpectedResult = 5_832)]
+        [TestCase(13, ExpectedResult = 23_514_624_000)]
+        public ulong Test_Solution(int x)
+        {
+            var sut = new Problem08();
+
+            var actual = sut.Solve(length: x);
+
+            TestContext.WriteLine(actual);
+            return actual;
+        }
+    }
+}
